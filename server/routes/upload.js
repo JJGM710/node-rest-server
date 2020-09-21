47c78e4ef5f1e5789a439500972b3179db835a77
Juan Giler <juangiler7@outlook.com>
2020-09-21 17:28:47 -0500

    1223

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -6,11 +6,11 @@ const path = require('path');
 const Usuario = require('../models/usuario');
 const Producto = require('../models/producto');
 
-// const {
-// 	imagenProducto,
-// 	imagenUsuario,
-// 	borrarArchivo,
-// } = require('../helpers/uploadImage');
+const {
+	imagenProducto,
+	imagenUsuario,
+	borrarArchivo,
+} = require('../helpers/uploadImage');
 
 app.use(fileUpload());
 
@@ -90,95 +90,5 @@ app.put('/upload/:tipo/:id', function (req, res) {
 		}
 	});
 });
-const borrarArchivo = (nombreImagen, tipo) => {
-	let pathUrl = path.resolve(
-		__dirname,
-		`../../uploads/${tipo}/${nombreImagen}`
-	);
-
-	//borra una imagen
-	if (fs.existsSync(pathUrl)) {
-		fs.unlinkSync(pathUrl);
-	}
-};
-
-const imagenProducto = (id, res, nombreArchivo) => {
-	Producto.findById(id, (err, productoDB) => {
-		if (err) {
-			borrarArchivo(nombreArchivo, 'productos');
-			return res.status(500).json({
-				ok: false,
-				err,
-			});
-		}
-
-		if (!productoDB) {
-			borrarArchivo(nombreArchivo, 'productos');
-			return res.status(400).json({
-				ok: false,
-				err,
-			});
-		}
-
-		borrarArchivo(productoDB.img, 'productos');
-
-		productoDB.img = nombreArchivo;
-
-		productoDB.save((err, productoSave) => {
-			if (err) {
-				return res.status(500).json({
-					ok: false,
-					err,
-				});
-			}
-
-			res.json({
-				ok: true,
-				producto: productoSave,
-				img: nombreArchivo,
-			});
-		});
-	});
-};
-
-const imagenUsuario = (id, res, nombreArchivo) => {
-	Usuario.findById(id, (err, usuarioDB) => {
-		if (err) {
-			borrarArchivo(nombreArchivo, 'usuarios');
-			return res.status(500).json({
-				ok: false,
-				err,
-			});
-		}
-
-		if (!usuarioDB) {
-			borrarArchivo(nombreArchivo, 'usuarios');
-			return res.status(400).json({
-				ok: false,
-				err,
-			});
-		}
-
-		//se asigna el path del archivo anterior unico para id
-		borrarArchivo(usuarioDB.img, 'usuarios');
-
-		usuarioDB.img = nombreArchivo;
-
-		usuarioDB.save((err, usuarioSave) => {
-			if (err) {
-				return res.status(500).json({
-					ok: false,
-					err,
-				});
-			}
-
-			res.json({
-				ok: true,
-				usuario: usuarioSave,
-				img: nombreArchivo,
-			});
-		});
-	});
-};
 
 module.exports = app;
